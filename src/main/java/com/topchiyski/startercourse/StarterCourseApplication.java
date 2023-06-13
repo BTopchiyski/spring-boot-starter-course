9e40914b13cd89ff0bd4191c2cac4899d0b517cb
BTopchiyski <topchiisky@gmail.com>
2023-06-14 01:37:24 +0300

    Initializing spring boot project

---
+++ src/main/java/com/topchiyski/startercourse/StarterCourseApplication.java
@@ -0,0 +1,13 @@
+package com.topchiyski.startercourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/topchiyski/startercourse/StartercourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topchiyski.startercourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartercourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
